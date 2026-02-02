8e018208d64c5a86713a6e97235faa94a6a64864
gopikaJS28 <gopikajayaramsheeba@gmail.com>
2026-02-02 16:39:36 +0530

    requiremnts added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical-chatbot",
+    version="0.1",
+    author="gopika",
+    email="gopika@example.com",
+    packages=find_packages(),
+    install_requires=[]
+)
